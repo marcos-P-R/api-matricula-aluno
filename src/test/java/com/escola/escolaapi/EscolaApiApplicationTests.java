c4fcc60f8020764b532928301e0f900a44b696e0
Marcos P <marcos.p.mcruz@outlook.com>
2022-05-12 19:10:37 -0300

    Iniciando o projeto
    Criando o projeto de api para o sistema da escola

---
+++ src/test/java/com/escola/escolaapi/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escola.escolaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
